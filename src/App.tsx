c207ec60b91d9347093d5bf1a93fcf6b85f8d956
Ariiychyk Denis <areychyk@mail.ru>
2022-12-21 22:18:16 +0300

    lesson 3

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,19 @@ import React from 'react';
 import './App.css';
 
 import {Lesson_3_App} from "./Lessons/Lesson_3/Lesson_3_App";
-import {ComponentsApp} from "./PS/PS_2/ComponentsApp";
+// import {ComponentsApp_2} from "./PS/PS_2/ComponentsApp_2";
+import ComponentsApp_3 from "./PS/PS_3/ComponentsApp_3";
 
 
 
 
 function App() {
-    return (
-        // <Lesson_2_App/>
-        <Lesson_3_App/>
+    return (<>
+        {/* <Lesson_2_App/>*/}
+        {/* <Lesson_3_App/>*/}
+        <ComponentsApp_3/>
 
-    );
+    </> );
 }
 
 export default App;
