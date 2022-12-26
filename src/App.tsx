0c8385e79987dd5b6a6c5ac6955b7cb6a7def8ea
Ariiychyk Denis <areychyk@mail.ru>
2022-12-26 22:16:07 +0300

    les 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,63 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import {Todolist} from './Todolist';
+import { v1 } from 'uuid';
 
-import {Lesson_3_App} from "./Lessons/Lesson_3/Lesson_3_App";
-// import {ComponentsApp_2} from "./PS/PS_2/ComponentsApp_2";
-import ComponentsApp_3 from "./PS/PS_3/ComponentsApp_3";
-
-
-
+export type FilterValuesType = "all" | "active" | "completed";
 
 function App() {
-    return (<>
-        {/* <Lesson_2_App/>*/}
-        {/* <Lesson_3_App/>*/}
-        <ComponentsApp_3/>
 
-    </> );
+    let [tasks, setTasks] = useState([
+        { id: v1(), title: "HTML&CSS", isDone: true },
+        { id: v1(), title: "JS", isDone: true },
+        { id: v1(), title: "ReactJS", isDone: false },
+        { id: v1(), title: "Rest API", isDone: false },
+        { id: v1(), title: "GraphQL", isDone: false },
+    ]);
+
+    function removeTask(id: string) {
+        let filteredTasks = tasks.filter(t => t.id != id);
+        setTasks(filteredTasks);
+    }
+
+    function addTask(title: string) {
+        let task = { id: v1(), title: title, isDone: false };
+        let newTasks = [task, ...tasks];
+        setTasks(newTasks);
+    }
+
+    const checkBoxChange=(newId: string, checkValue:boolean)=>{
+        setTasks( tasks.map(el=>el.id===newId ? {...el, isDone:checkValue }:el))
+    }
+
+    let [filter, setFilter] = useState<FilterValuesType>("all");
+
+    let tasksForTodolist = tasks;
+
+    if (filter === "active") {
+        tasksForTodolist = tasks.filter(t => t.isDone === false);
+    }
+    if (filter === "completed") {
+        tasksForTodolist = tasks.filter(t => t.isDone === true);
+    }
+
+    function changeFilter(value: FilterValuesType) {
+        setFilter(value);
+    }
+
+
+
+    return (
+        <div className="App">
+            <Todolist title="What to learn"
+                      tasks={tasksForTodolist}
+                      removeTask={removeTask}
+                      changeFilter={changeFilter}
+                      addTask={addTask}
+                      checkBoxChange={checkBoxChange}
+            />
+        </div>
+    );
 }
 
 export default App;
